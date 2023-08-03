26bc127e5f44d83ee78fcd591e7682e64e3f8ec4
bc-stud-577504 <112558379+bc-stud-577504@users.noreply.github.com>
2023-08-03 10:44:56 +0200

    Update load_projects.js

--- load_projects.js
+++ load_projects.js
@@ -1,24 +1,9 @@
 
-const projects = {
-    1: {id:1, name:"project1", tickets: 2},
-    2: {id:2, name:"project2", tickets: 1}
-};
+//import {getLocalStorageItem} from "./io";
 
-const users = {
-    1: {id: 1, name: "user1", surname: "surname", age: 1},
-    2: {id: 2, name: "user2", surname: "surname", age: 1}
-};
+projects = getLocalStorageItem("Projects");
 
-const tickets = [
-    {summary:"summary", detailed:"detailed", created_by: 1, created: new Date(), project: 1, assigned_to: 2,
-    priority: "low", target: new Date(), actual: new Date(), res_summary: "summary", status:"closed"},
-
-    {summary:"summary", detailed:"detailed", created_by: 2, created: new Date(), project: 2, assigned_to: 1,
-    priority: "medium", target: new Date(), actual: null, res_summary: null, status:"open"},
-
-    {summary:"summary", detailed:"detailed", created_by: 1, created: new Date(), project: 1, assigned_to: null,
-    priority: "low", target: new Date(), actual: new Date(), res_summary: "summary", status:"closed"}
-];
+console.log(projects)
 
 function create_project_block(indx, dict) {
     return `<div onclick="location.href='/project.html?id=${indx}'" class='project' id='project${indx}'>
@@ -31,8 +16,6 @@ for (let x of Object.keys(projects)) {
     document.getElementById("projects").innerHTML += create_project_block(x, projects);
 }
 
-
-
 // summary, 
 // detailed, 
 // created_by, 
@@ -44,4 +27,3 @@ for (let x of Object.keys(projects)) {
 // actual date,
 // resolution summary,
 // status
-
