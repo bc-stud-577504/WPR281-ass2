a810d453b83932621a18c3c27c214d695f87a002
Neill Engelbrecht <577504@student.belgiumcampus.ac.za>
2023-08-04 12:20:24 +0200

    made the html pages recerence the correct .css file

--- load_projects.js
+++ load_projects.js
@@ -5,13 +5,13 @@ projects = getLocalStorageItem("Projects");
 
 console.log(projects)
 
-function create_project_block(indx, dict) {
-    return `<div onclick="location.href='/project.html?id=${indx}'" class='project' id='project${indx}'>
-                <p><b>${dict[indx].name}</b></p>
-                <p>number of tickets: ${dict[indx].tickets}</p>
+function create_project_block(key, dict) {
+    return `<div onclick="location.href='/project.html?id=${key}'" class='project' id='project${key}'>
+                <p><b>${dict[key].name}</b></p>
+                <p>number of tickets: ${dict[key].tickets}</p>
             </div>`;
 }
 
-for (let x of Object.keys(projects)) {
-    document.getElementById("projects").innerHTML += create_project_block(x, projects);
+for (let key in projects) {
+    document.getElementById("projects").innerHTML += create_project_block(key, projects);
 }
