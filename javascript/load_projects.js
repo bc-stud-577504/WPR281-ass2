737ecb4fb1d4d49b9b11d60a72812a6f24a1d1f0
Neill Engelbrecht <577504@student.belgiumcampus.ac.za>
2023-08-04 13:16:31 +0200

    added file structure

--- javascript/load_projects.js
+++ javascript/load_projects.js
@@ -6,7 +6,7 @@ projects = getLocalStorageItem("Projects");
 console.log(projects)
 
 function create_project_block(indx, dict) {
-    return `<div onclick="location.href='/project.html?id=${indx}'" class='project' id='project${indx}'>
+    return `<div onclick="location.href='/pages/project.html?id=${indx}'" class='project' id='project${indx}'>
                 <p><b>${dict[indx].name}</b></p>
                 <p>number of tickets: ${dict[indx].tickets}</p>
             </div>`;
