4ad9b9454304bf56487374adccd8f7d411cd41e2
Neill Engelbrecht <577504@student.belgiumcampus.ac.za>
2023-08-07 16:14:52 +0200

    .

--- javascript/utils/ticket.js
+++ javascript/utils/ticket.js
@@ -6,8 +6,11 @@ function Ticket(id, summary, detailed) {
 }
 
 function createTicketBlock(ticket) {
+
+    let user = getLocalStorageItem("Users")[ticket.assigned_to];
+
     return `<div onclick="location.href='/pages/ticket.html?id=${ticket.id}'" class='ticket' id='ticket${ticket.id}'>
                 <p><b>${ticket.summary}</b></p>
-                <p>Assigned to: ${ticket.assigned_to != null? ticket.assigned_to : "nobody"}</p>
+                <p>Assigned to: ${(ticket.assigned_to!=null)?user.name:"nobody"}</p>
             </div>`;
 }
